5cedcc3d985c37cf42351fd52655142f79cff74e
Andrew Mleczko <andrew@mleczko.net>
2013-02-19 17:29:32 +0100

    initial release

---
+++ rt/simpleslider/README.txt
@@ -0,0 +1,14 @@
+rt.simpleslider Package Readme
+=========================
+
+Overview
+--------
+
+A really simple Plone slider based on basic-slider.com
+
+
+Your tests here
+---------------
+
+    >>> 1 + 1
+    3
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='rt.simpleslider',
+      version=version,
+      description="A really simple Plone slider based on basic-slider.com",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone slider',
+      author='RedTurtle team',
+      author_email='svilplone@redturtle.it',
+      url='http://www.redturtle.it',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['rt'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
