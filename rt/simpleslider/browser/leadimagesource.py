1a81be927fed8ce8006cd39091b7d87a54a7018e
Andrew Mleczko <andrew@mleczko.net>
2013-02-20 14:04:49 +0100

    add viewlet hide for cl

--- rt/simpleslider/browser/leadimagesource.py
+++ rt/simpleslider/browser/leadimagesource.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-
+import os
 from zope.component import adapts, getMultiAdapter
 from zope.interface import implements
 from zope.publisher.interfaces.browser import IBrowserView
 from Products.Archetypes.interfaces.base import IBaseObject
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.contentleadimage.interfaces import ILeadImageable,\
      ILeadImageSpecific
 from collective.contentleadimage.config import IMAGE_FIELD_NAME
 from collective.contentleadimage.config import IMAGE_CAPTION_FIELD_NAME
+from collective.contentleadimage.browser.viewlets import LeadImageViewlet as BaseViewlet
+from collective.contentleadimage.browser import viewlets
 
 from rt.simpleslider.interfaces import ISliderSource
+from rt.simpleslider.vocabularies import SLIDER_MYSELF
 
 
 class ContentLeadImageSliderSource(object):
@@ -52,3 +56,27 @@ class ContentLeadImageSliderSource(object):
                                      ISliderSource)
             img = slider.getImage()
             yield img
+
+
+CLPATH = os.path.dirname(viewlets.__file__)
+
+
+class LeadImageViewletFull(BaseViewlet):
+    index = ViewPageTemplateFile('%s/leadimage-body.pt' % CLPATH)
+
+    def render(self):
+        slider_tool = getattr(self.request,'slider_tool',None)
+        if not slider_tool:
+            return super(LeadImageViewletFull, self).render()
+        if not slider_tool.show_slider():
+            return super(LeadImageViewletFull, self).render()
+
+        field = self.context.getField('show_slider')
+        if field and field.get(self.context) == SLIDER_MYSELF:
+            return ''
+        else:
+            return super(LeadImageViewletFull, self).render()
+
+
+class LeadImageViewletThumb(LeadImageViewletFull):
+    index = ViewPageTemplateFile('%s/leadimage.pt' % CLPATH)
